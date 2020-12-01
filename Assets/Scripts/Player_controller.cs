41ee688ae8168aa1fb34657fccb13fbaa733a395
Lawrence Zhu <lawrence.zhu@coderclass.nl>
2020-12-01 15:01:51 +0100

    enum en states

--- Assets/Scripts/Player_controller.cs
+++ Assets/Scripts/Player_controller.cs
@@ -7,14 +7,23 @@ public class Player_controller : MonoBehaviour
     [SerializeField] public LayerMask groundLayerMask;
     public float jumpHeight;
     public float speed;
+    
     private Rigidbody2D rigidbody2d;
     private BoxCollider2D boxCollider2D;
     private SpriteRenderer Sprite;
     public Animator animator;
-    enum States
-    {
-        Walking,
+    public enum CharacterState
+    { 
+        Idle,
         Jumping,
+        Walking,
+        InAir
+    }
+    public CharacterState characterState;
+
+    void Awake()
+    {
+        characterState = CharacterState.Idle;
     }
     void Start()
     {
@@ -23,37 +32,75 @@ public class Player_controller : MonoBehaviour
         Sprite = transform.GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-
     void Update()
-    {  
-        float move = Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(move, 0f, 0f);
-        if (move<0&& Sprite != null){
-                Sprite.flipX = true;
-        }
-        if (move >0)
-        {
-            Sprite.flipX = false;
-        }
-        transform.position += movement  * Time.deltaTime* speed;
-        animator.SetFloat("Speed", Mathf.Abs(move));
-        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
-        {
-            animator.SetBool("IsJumping", true);
-            rigidbody2d.AddForce (transform.up * jumpHeight, ForceMode2D.Impulse);
-        }
+    {
+        AnimatorGroundCheck();
+        switch (characterState)
+            {
+                case CharacterState.Idle:
+                    Idle();
+                    if (Input.GetAxisRaw("Horizontal") != 0)
+                    {
+                        characterState = CharacterState.Walking;
+                    }
+                    if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+                    {
+                        characterState = CharacterState.Jumping;
+                    }
+                    break;
+                case CharacterState.Jumping:
+                    Jumping();
+                    characterState = CharacterState.Idle;
+                    break;
+                case CharacterState.Walking:
+                    Walking();
+                    if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+                        {
+                            characterState = CharacterState.Jumping;
+                        }
+                    break; 
+            }
+
+        Debug.Log(characterState);
+    }
+
+    void AnimatorGroundCheck()
+    {
         if (IsGrounded())
         {
             animator.SetBool("IsJumping", false);
         }
-        else
+        else if (IsGrounded() == false)
         {
             animator.SetBool("IsJumping", true);
         }
+
+
     }
+    void Idle()
+    {
 
-
+    }
+    void Jumping()
+    {
+        animator.SetBool("IsJumping", true);
+        rigidbody2d.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+    }
+    void Walking()
+    {
+        float move = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(move, 0f, 0f);
+        if (move < 0 && Sprite != null)
+        {
+            Sprite.flipX = true;
+        }
+        if (move > 0)
+        {
+            Sprite.flipX = false;
+        }
+        transform.position += movement * Time.deltaTime * speed;
+        animator.SetFloat("Speed", Mathf.Abs(move));
+    }
     private bool IsGrounded()
     {
         float Height = 0.1f;
