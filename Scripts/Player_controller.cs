c2e0a1055072ec93767fd63164107e7648fcf802
Lawrence Zhu <lawrence.zhu@coderclass.nl>
2021-02-18 12:57:48 +0100

    alles?

---
+++ Scripts/Player_controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_controller : MonoBehaviour
+{
+    public float jumpHeight;
+    public float speed;
+    private Rigidbody2D rigidbody2d;
+    enum States
+    {
+        Walking,
+        Jumping,        
+    }
+    void Start()
+    {
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position += movement * Time.deltaTime * speed;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody2d.velocity = Vector2.up * jumpHeight;
+        }
+
+
+
+
+    }
+    
+}
